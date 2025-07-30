398513e0c7dd81ccc6076534cb4cb7ba097016d8
shokhruzisroilov <shokhruzisroilov@gmail.com>
2025-07-30 11:04:37 +0500

    fix

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,34 +1,27 @@
 import Header from '@/components/public/home/Header'
-import Navbar from '@/components/public/common/Navbar'
 import Function from '@/components/public/home/Function'
 import About from '@/components/public/home/About'
 import Statistic from '@/components/public/home/Statistic'
-import Footer from '@/components/public/common/Footer'
 import Interface from '@/components/public/home/Interface'
 import Contact from '@/components/public/home/Contact'
-import AOSWrapper from '@/components/public/common/AOSWrapper'
 
 export default function HomePage() {
 	return (
-		<AOSWrapper>
-			<div>
-				<Navbar />
-				<section>
-					<Header />
-				</section>
-				<section id='about'>
-					<About />
-				</section>
-				<section id='news'>
-					<Function />
-				</section>
-				<section id='contact'>
-					<Contact />
-				</section>
-				<Interface />
-				<Statistic />
-				<Footer />
-			</div>
-		</AOSWrapper>
+		<div className='overflow-x-hidden'>
+			<section>
+				<Header />
+			</section>
+			<section id='about'>
+				<About />
+			</section>
+			<section id='news'>
+				<Function />
+			</section>
+			<section id='contact'>
+				<Contact />
+			</section>
+			<Interface />
+			<Statistic />
+		</div>
 	)
 }
